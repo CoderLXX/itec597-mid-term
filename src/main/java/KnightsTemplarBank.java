652244e70b0892491b16ac0edbf98e64be89417a
Jeremy Liu <jeremyliu153@gmail.com>
2019-05-26 21:10:56 -0700

    第三题解答

--- src/main/java/KnightsTemplarBank.java
+++ src/main/java/KnightsTemplarBank.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class KnightsTemplarBank {
+
 
     /**
      * 返回圣殿骑士团银行职员可有的兑换方法总数以及
@@ -8,7 +11,57 @@ public class KnightsTemplarBank {
      * @param options 可以使用的面值，最小值未必是1，无特定排列顺序
      */
     public int[] resolve(int amount, int[] options) {
-        return null;
+        Arrays.sort(options);
+
+        int[][] dp = new int[amount + 1][options.length];
+        for (int i = options[0]; i <= amount; i++) {
+            dp[i][0] = i % options[0] == 0 ? 1 : 0;
+        }
+        for (int i = 0; i < options.length; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = options[0]; i <= amount; i++) {
+            for (int j = 1; j < options.length; j++) {
+                for (int k = i; k >= 0; k -= options[j]) {
+                    dp[i][j] += dp[k][j - 1];
+                }
+            }
+        }
+
+
+        int all = dp[amount][options.length - 1];
+        int minOfMethod = -1;
+        if (all > 0) {
+
+        if(amount<1) return null;
+        int[] dp2 = new int[amount+1];
+        int sum = 0;
+
+        while(++sum<=amount) {
+            int min = -1;
+            for(int coin : options) {
+                if(sum >= coin && dp2[sum-coin]!=-1) {
+                    int temp = dp2[sum-coin]+1;
+                    min = min<0 ? temp : (temp < min ? temp : min);
+                }
+            }
+            dp2[sum] = min;
+        }
+            minOfMethod = dp2[amount];
+        } else {
+            all = -1;
+        }
+
+        System.out.println(all + "  " + minOfMethod);
+        return new int[]{all, minOfMethod};
+    }
+
+
+    public static void main(String[] args) {
+        KnightsTemplarBank ktBank = new KnightsTemplarBank();
+        int[] options = {1, 2, 3, 4};
+        ktBank.resolve(4, options);
     }
 
 }
